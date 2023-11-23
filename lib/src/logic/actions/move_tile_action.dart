2beb44287bb091fec693e544fbaf30003c280f86
Simeon Steward <simeontsteward@gmail.com>
2023-11-23 15:30:13 -0600

    added move tile

---
+++ lib/src/logic/actions/move_tile_action.dart
@@ -0,0 +1,51 @@
+import 'package:link_five/src/logic/actions/action_helpers.dart';
+import 'package:link_five/src/logic/game_action.dart';
+import 'package:link_five/src/model/game/game_state.dart';
+import 'package:link_five/src/model/game/player_color.dart';
+import 'package:link_five/src/model/game/tile.dart';
+import 'package:link_five/src/model/game/tile_location.dart';
+
+class MoveTileAction extends GameAction {
+  TileLocation source;
+  TileLocation destination;
+
+  MoveTileAction({
+    required PlayerColor playerColor,
+    required this.source,
+    required this.destination,
+  }) : super(playerColor: playerColor);
+
+  @override
+  bool isPermitted(GameState gameState) {
+    final isGameBoardEmpty = gameState.tiles.isEmpty;
+    final isSourceTileCorrect =
+        gameState.tile(destination)!.color == playerColor;
+    final isLocationEmpty = gameState.tile(destination) == null;
+    final hasAdjacentTile = destination.hasAdjacentTile(gameState);
+    final tilesAvailable = gameState.tilesAvailable;
+
+    return isLocationEmpty &&
+        (hasAdjacentTile || isGameBoardEmpty) &&
+        !gameState.hasWinner &&
+        tilesAvailable &&
+        isSourceTileCorrect;
+  }
+
+  @override
+  GameState applyAction(GameState gameState) {
+    final tile = Tile(location: destination, color: playerColor);
+    gameState = gameState.placeTile(tile);
+    gameState = gameState.removeTile(source);
+
+    final winningTiles = findWin(gameState, tile);
+    if (winningTiles == null) {
+      return gameState.rebuild(
+        (b) => b..turnNumber = gameState.turnNumber + 1,
+      );
+    } else {
+      return gameState.rebuild(
+        (b) => b..winningTiles = winningTiles.toBuilder(),
+      );
+    }
+  }
+}
