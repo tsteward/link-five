1caf295b0cbe187367eadf0bbca1e6ec3b7597b3
Simeon Steward <simeontsteward@gmail.com>
2023-11-24 12:32:26 -0600

    Refactored handleGameClick for easier readability

--- lib/src/model/clicker/click_handler.dart
+++ lib/src/model/clicker/click_handler.dart
@@ -24,33 +24,27 @@ class ClickHandler {
         location: location,
       );
     } else {
-      final colorAtLocation = state.gameBoard[location]?.color;
-      if (_source == null) {
-        if (colorAtLocation == playerColor) {
-          _setSource(location);
-        }
-        return null;
-      } else {
-        if (colorAtLocation == playerColor) {
-          if (location == _source) {
-            _setSource(null);
-          } else {
-            _setSource(location);
-          }
-          return null;
-        } else if (colorAtLocation == null) {
-          final action = MoveTileAction(
-            playerColor: playerColor,
-            source: _source!,
-            destination: location,
-          );
-          _setSource(null);
-          return action;
-        } else {
-          return null;
-        }
-      }
+      return _handleMoveTileSelector(state, location, playerColor);
     }
+  }
+
+  MoveTileAction? _handleMoveTileSelector(
+      GameState state, TileLocation location, PlayerColor playerColor) {
+    final colorAtLocation = state.gameBoard[location]?.color;
+    if (colorAtLocation == null && _source != null) {
+      final action = MoveTileAction(
+        playerColor: playerColor,
+        source: _source!,
+        destination: location,
+      );
+      _setSource(null);
+      return action;
+    } else if (location == _source) {
+      _setSource(null);
+    } else if (colorAtLocation == playerColor) {
+      _setSource(location);
+    }
+    return null;
   }
 
   void _setSource(TileLocation? newState) {
