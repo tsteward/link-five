9a37d64ba04d36e25028a3125b6d2ba9eec9961a
Timothy Steward <tsteward@users.noreply.github.com>
2025-01-13 14:56:19 +0000

    withValues

--- lib/src/widgets/game_setup_container.dart
+++ lib/src/widgets/game_setup_container.dart
@@ -20,7 +20,7 @@ class GameSetupContainerWidget extends StatelessWidget {
               bottomRight: Radius.circular(10)),
           boxShadow: [
             BoxShadow(
-              color: Colors.grey.vithValues(alpha: 0.5),
+              color: Colors.grey.withValues(alpha: 0.5),
               spreadRadius: 5,
               blurRadius: 7,
               offset: const Offset(0, 3), // changes position of shadow
